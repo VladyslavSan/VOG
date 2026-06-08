b96422fb2dc5168e1af8b3dc39748a11b659bde5
Vladyslav Odobesku <odobesku.vladislav@gmail.com>
2026-06-08 17:26:20 +0300

    fix: suppress magic-number warnings on default window dimensions

--- src/VOG/Application/include/VOG/Application/Application.hpp
+++ src/VOG/Application/include/VOG/Application/Application.hpp
@@ -21,8 +21,8 @@ public:
     {
         // Generic application info.
         std::string   title  = "Application";
-        std::uint32_t width  = 640;
-        std::uint32_t height = 480;
+        std::uint32_t width  = 640u; // NOLINT(readability-magic-numbers)
+        std::uint32_t height = 480u; // NOLINT(readability-magic-numbers)
 
         // Vulkan extensions
         std::vector<std::string> extensions;
